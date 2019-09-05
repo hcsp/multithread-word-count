1f369e5d577b11091ea1f6cf87c63e56ceb30757
Suyu <zhuangsuyu1993@163.com>
2019-09-05 10:08:57 +0800

    使用线程池实现单词统计功能 (#2)
    使用线程池实现单词统计功能 (#2)

--- src/main/java/com/github/hcsp/multithread/WordCount.java
+++ src/main/java/com/github/hcsp/multithread/WordCount.java
@@ -1,13 +1,69 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class WordCount {
-    public WordCount(int threadNum) {}
+    private final int threadNum;
+    private ExecutorService threadPool;
+
+    public WordCount(int threadNum) {
+        threadPool = Executors.newFixedThreadPool(threadNum);
+        this.threadNum = threadNum;
+    }
 
     // 统计文件中各单词的数量
-    public Map<String, Integer> count(File file) {
-        return null;
+    public Map<String, Integer> count(File file) throws FileNotFoundException, ExecutionException, InterruptedException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        List<Future<Map<String, Integer>>> futureList = new ArrayList<>();
+        for (int i = 0; i < threadNum; i++) {
+            futureList.add(threadPool.submit(new WorkerJob(reader)));
+        }
+
+        Map<String, Integer> finalResult = new HashMap<>();
+        for (Future<Map<String, Integer>> future : futureList) {
+            mergeWorkerMapIntoFinalResult(future.get(), finalResult);
+        }
+
+        return finalResult;
+    }
+
+    class WorkerJob implements Callable<Map<String, Integer>> {
+        BufferedReader reader;
+
+        WorkerJob(BufferedReader reader) {
+            this.reader = reader;
+        }
+
+        @Override
+        public Map<String, Integer> call() throws Exception {
+            String line;
+            Map<String, Integer> result = new HashMap<>();
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    result.put(word, result.getOrDefault(word, 0) + 1);
+                }
+            }
+            return result;
+        }
+    }
+
+    private void mergeWorkerMapIntoFinalResult(Map<String, Integer> workerMap, Map<String, Integer> finalResult) {
+        for (Map.Entry<String, Integer> entry : workerMap.entrySet()) {
+            Integer mergedCount = finalResult.getOrDefault(entry.getKey(), 0) + entry.getValue();
+            finalResult.put(entry.getKey(), mergedCount);
+        }
     }
 }
