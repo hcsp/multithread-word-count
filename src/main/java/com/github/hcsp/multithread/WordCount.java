81e109bd61b87d3e954d1d7c9ed6c178241dfb8d
JiangXi-YuYan <51253421+JiangXi-YuYan@users.noreply.github.com>
2019-09-12 17:16:35 +0800

    单个线程的蠢办法实现该功能。 (#7)
    单个线程的蠢办法实现该功能。 (#7)

--- src/main/java/com/github/hcsp/multithread/WordCount.java
+++ src/main/java/com/github/hcsp/multithread/WordCount.java
@@ -1,13 +1,52 @@
 package com.github.hcsp.multithread;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class WordCount {
-    public WordCount(int threadNum) {}
+    public WordCount(int threadNum) {
+    }
 
     // 统计文件中各单词的数量
-    public Map<String, Integer> count(File file) {
-        return null;
+    public Map<String, Integer> count(File file) throws IOException {
+        Map<String, Integer> numberOfWord = new HashMap<>();
+        StringTokenizer stringTokenizer = dealWithStringFromFile(file);
+        while (stringTokenizer.hasMoreTokens()) {
+            String aWord = stringTokenizer.nextToken();
+            if (numberOfWord.containsKey(aWord)) {
+                int count = numberOfWord.get(aWord);
+                numberOfWord.put(aWord, count + 1);
+            } else {
+                numberOfWord.put(aWord, 1);
+            }
+        }
+
+        System.out.println(numberOfWord);
+        return numberOfWord;
+    }
+
+    /**
+     * Processing the content of the file into a string
+     *
+     * @param file A file whose content is a character
+     * @return StringTokenizer
+     * @throws IOException File is empty
+     */
+
+    public static StringTokenizer dealWithStringFromFile(File file) throws IOException {
+        List<String> stringFromFile = Files.readAllLines(file.toPath());
+        StringTokenizer string;
+        string = new StringTokenizer(stringFromFile.toString()
+                .replace("[", " ")
+                .replace("]", " ")
+                .replace(",", " "));
+        return string;
+
     }
 }
+
