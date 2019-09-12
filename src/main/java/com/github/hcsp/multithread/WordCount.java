24c48e664276f8b66881559e3504c90e39697eae
Bo Zhang <zhangbodut@gmail.com>
2019-09-12 23:01:24 +0800

    Use list for word count

--- src/main/java/com/github/hcsp/multithread/WordCount.java
+++ src/main/java/com/github/hcsp/multithread/WordCount.java
@@ -1,13 +1,14 @@
 package com.github.hcsp.multithread;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
 
 public class WordCount {
     public WordCount(int threadNum) {}
 
     // 统计文件中各单词的数量
-    public Map<String, Integer> count(File file) {
+    public Map<String, Integer> count(List<File> file) {
         return null;
     }
 }
--- src/test/java/com/github/hcsp/multithread/WordCountTest.java
+++ src/test/java/com/github/hcsp/multithread/WordCountTest.java
@@ -4,46 +4,51 @@ import com.google.common.collect.Lists;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WordCountTest {
+    private static final int REPEAT = 100;
+
     @Test
     public void test() throws Exception {
-        List<String> aToZ =
+        // 2600 lines of random aligned strings
+        List<String> randomStrings =
                 IntStream.range(0, 26)
                         .mapToObj(i -> String.valueOf((char) ('a' + i)))
+                        .flatMap(letter -> Collections.nCopies(REPEAT, letter).stream())
                         .collect(Collectors.toList());
-        Integer count = new Random().nextInt(10) + 100;
-        List<String> randomStrings = new ArrayList<>();
-        for (String letter : aToZ) {
-            randomStrings.addAll(Collections.nCopies(count, letter));
-        }
 
         Collections.shuffle(randomStrings);
 
-        File file = writeToTempFile(count, randomStrings);
-
+        // Write to 10 files
+        List<File> files =
+                Lists.partition(randomStrings, 26 * REPEAT / 10).stream()
+                        .map(this::writeToTempFile)
+                        .collect(Collectors.toList());
         WordCount wordCount = new WordCount(10);
-        Map<String, Integer> countResult = wordCount.count(file);
+        Map<String, Integer> countResult = wordCount.count(files);
         System.out.println(countResult);
-        Assertions.assertTrue(countResult.values().stream().allMatch(count::equals));
+        Assertions.assertTrue(countResult.values().stream().allMatch(n -> n == REPEAT));
     }
 
-    private File writeToTempFile(int count, List<String> randomStrings) throws IOException {
-        File file = File.createTempFile("tmp", "");
-        List<String> lines =
-                Lists.partition(randomStrings, 10).stream()
-                        .map(list -> String.join(" ", list))
-                        .collect(Collectors.toList());
-        Files.write(file.toPath(), lines);
-        return file;
+    private File writeToTempFile(List<String> oneCharacterLines) {
+        try {
+            File file = File.createTempFile("tmp", "");
+
+            List<String> lines =
+                    Lists.partition(oneCharacterLines, 10).stream()
+                            .map(list -> String.join(" ", list))
+                            .collect(Collectors.toList());
+            Files.write(file.toPath(), lines);
+            return file;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
