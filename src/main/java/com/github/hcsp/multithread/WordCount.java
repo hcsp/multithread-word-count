ee933fde5a9ac9918db46ff25a1b6e62567c7abb
Yuyan <845058547@qq.com>
2019-09-15 10:13:16 +0800

    调了好久，还是不知道怎么解决当前的问题。

--- src/main/java/com/github/hcsp/multithread/WordCount.java
+++ src/main/java/com/github/hcsp/multithread/WordCount.java
@@ -1,14 +1,13 @@
 package com.github.hcsp.multithread;
 
-import com.sun.deploy.security.SelectableSecurityManager;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -17,8 +16,9 @@ import java.util.concurrent.Future;
 public class WordCount {
     private final int threadNum;
     private ExecutorService threadPool;
+    private final Object lock = new Object();
 
-    public WordCount(int threadNum) throws ExecutionException, InterruptedException {
+    public WordCount(int threadNum) {
         this.threadNum = threadNum;
         threadPool = Executors.newFixedThreadPool(threadNum);
     }
@@ -26,35 +26,48 @@ public class WordCount {
 
     // 统计文件中各单词的数量
     public Map<String, Integer> count(List<File> files) throws ExecutionException, InterruptedException {
-        List<Future<Map<String, Integer>>> list = new ArrayList<>();
-        Map<String, Integer> finalResult = new HashMap<>();
-        for (int i = 0; i < threadNum; ++i) {
-            while (files.get(0).exists()) {
-                File file = files.get(0);
-                list.add(threadPool.submit(() -> {
-                    Map<String, Integer> result = new HashMap<>();
+        synchronized (lock) {
+            List<Future<Map<String, Integer>>> list = new ArrayList<>();
+            Map<String, Integer> finalResult = new ConcurrentHashMap<>();
+            for (int i = 0; i < threadNum; ++i) {
+                list.add(threadPool.submit(
+                        new Worker(averageList(files, threadNum).get(i))));
+            }
+
+            for (Future<Map<String, Integer>> future : list
+            ) {
+                Map<String, Integer> resultFromThread = future.get();
+                mergeResultFromThread(resultFromThread, finalResult);
+            }
+            System.out.println(finalResult);
+            threadPool.shutdown();
+            return finalResult;
+        }
+    }
+
+    static class Worker implements Callable<Map<String, Integer>> {
+        List<File> files;
+
+        public Worker(List<File> files) {
+            this.files = files;
+        }
+
+        @Override
+        public Map<String, Integer> call() throws Exception {
+            Map<String, Integer> result = new ConcurrentHashMap<>();
+            while (!files.isEmpty()) {
+                for (File file : files) {
                     List<String> oneFileToString = Files.readAllLines(file.toPath());
                     for (String content : oneFileToString) {
                         String[] contentToArray = content.split(" ");
-                        for (String element : contentToArray
-                        ) {
+                        for (String element : contentToArray) {
                             result.put(element, result.getOrDefault(element, 0) + 1);
                         }
                     }
-                    files.remove(0);
-                    threadPool.shutdown();
-                    return result;
-                }));
+                }
             }
-            break;
+            return result;
         }
-        for (Future<Map<String, Integer>> future : list
-        ) {
-            Map<String, Integer> resultFromThread = future.get();
-            mergeResultFromThread(resultFromThread, finalResult);
-        }
-        System.out.println(finalResult);
-        return finalResult;
     }
 
     private void mergeResultFromThread(Map<String, Integer> resultFromThread,
@@ -65,13 +78,31 @@ public class WordCount {
         }
     }
 
+    private synchronized static List<List<File>> averageList(List<File> files, Integer number) {
+        List<List<File>> aPartOfList = new ArrayList<>();
+        int remainder = files.size() % number;
+        int division = files.size() / number;
+        int offset = 0;
+        for (int i = 0; i < number; i++) {
+            List<File> list;
+            if (remainder > 0) {
+                list = files.subList(i * division + offset, (i + 1) * division + offset + 1);
+                remainder--;
+            } else {
+                list = files.subList(i * division, (i + 1) * division);
+            }
+            aPartOfList.add(list);
+        }
+        return aPartOfList;
+    }
+
     public static void main(String[] args) throws ExecutionException, InterruptedException, FileNotFoundException {
         List<File> files = new ArrayList<>();
         File file = new File("C:\\Users\\catsme\\IdeaProjects\\multithread-word-count2\\1.txt");
         File file1 = new File("C:\\Users\\catsme\\IdeaProjects\\multithread-word-count2\\2.txt");
         files.add(file);
         files.add(file1);
-        WordCount wordCount = new WordCount(10);
+        WordCount wordCount = new WordCount(2);
         wordCount.count(files);
     }
 }
