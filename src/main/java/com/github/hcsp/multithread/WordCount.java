cf521b24d9b53b39ba00387dd02f2a531e1a617a
wangpeng1994 <947034046@qq.com>
2019-10-15 16:11:31 +0800

    Multithread word count.

--- src/main/java/com/github/hcsp/multithread/WordCount.java
+++ src/main/java/com/github/hcsp/multithread/WordCount.java
@@ -1,14 +1,20 @@
 package com.github.hcsp.multithread;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class WordCount {
 
@@ -20,14 +26,20 @@ public class WordCount {
         this.threadNum = threadNum;
     }
 
-    // 统计文件中各单词的数量
-    public Map<String, Integer> count(List<File> file) throws FileNotFoundException, ExecutionException, InterruptedException {
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+    /**
+     * 统计文件中各单词的数量
+     *
+     * @param files 文件列表
+     * @return 单词统计结果
+     * @throws IOException          文件读写出错时抛出
+     * @throws ExecutionException   Future 取回结果出错时抛出
+     * @throws InterruptedException 线程被中断时抛出
+     */
+    public Map<String, Integer> count(List<File> files) throws IOException, ExecutionException, InterruptedException {
+        BufferedReader reader = new BufferedReader(new FileReader(mergeFilesIntoSingleFile(files)));
         List<Future<Map<String, Integer>>> futures = new ArrayList<>();
-
         // 开辟若干个线程，每个线程读取文件的一行内容，并将单词统计结果返回
         // 最后主线程将工作线程返回的结果汇总在一起
-        // 似乎即使开了10个线程，但每个线程其实做了一样的事情，都是从头读到尾...
         for (int i = 0; i < threadNum; i++) {
             futures.add(threadPool.submit(new WorkerJob(reader)));
         }
@@ -42,6 +54,28 @@ public class WordCount {
         return finalResult;
     }
 
+    /**
+     * 将文件列表中的文件合并为一个文件
+     *
+     * @param files 文件列表
+     * @return 结果文件
+     * @throws IOException 文件读写出错时抛出
+     */
+    private File mergeFilesIntoSingleFile(List<File> files) throws IOException {
+        File result = File.createTempFile("tmp", "");
+        for (File file : files) {
+            String encoding = "UTF-8";
+            FileUtils.write(result, FileUtils.readFileToString(file, encoding), encoding, true);
+        }
+        return result;
+    }
+
+    /**
+     * 将子集合并到终集中
+     *
+     * @param resultFromWorker 终集
+     * @param finalResult      子集
+     */
     private void mergeWorkerResultIntoFinalResult(Map<String, Integer> resultFromWorker,
                                                   Map<String, Integer> finalResult) {
         for (Map.Entry<String, Integer> entry : resultFromWorker.entrySet()) {
@@ -64,9 +98,9 @@ public class WordCount {
             Map<String, Integer> result = new HashMap<>();
             while ((line = reader.readLine()) != null) {
                 String[] words = line.split(" ");
-                System.out.println(Thread.currentThread().getName());
-                System.out.println(line);
-                System.out.println();
+//                System.out.println(Thread.currentThread().getName());
+//                System.out.println(line);
+//                System.out.println();
                 for (String word : words) {
                     result.put(word, result.getOrDefault(word, 0) + 1);
                 }
@@ -74,48 +108,4 @@ public class WordCount {
             return result;
         }
     }
-
-    public static void main(String[] args) throws FileNotFoundException, ExecutionException, InterruptedException {
-        File file = new File("C:\\Users\\admin\\Projects\\tmp\\multithread-word-count\\src\\main\\java\\com\\github\\hcsp\\multithread\\testWords2.txt");
-        WordCount wordCount = new WordCount(10);
-        long t0 = System.currentTimeMillis();
-        Map<String, Integer> countResult = wordCount.count(file);
-        long t1 = System.currentTimeMillis();
-        System.out.println(countResult);
-        System.out.println(t1 - t0);
-    }
-
-//    public static void main(String[] args) throws ExecutionException, InterruptedException {
-//        ExecutorService threadPool = Executors.newFixedThreadPool(10);
-//
-//        // submit 方法会立刻返回（参考 JS 中的 promise）
-//        // 提交的任务会异步执行，不会阻塞当前线程
-//        Future<Integer> future1 = threadPool.submit(new Callable<Integer>() {
-//            @Override
-//            public Integer call() throws Exception {
-//                Thread.sleep(3000);
-//                return 0;
-//            }
-//        });
-//
-//        Future<String> future2 = threadPool.submit(new Callable<String>() {
-//            @Override
-//            public String call() throws Exception {
-//                Thread.sleep(1000);
-//                return "ojbk";
-//            }
-//        });
-//
-//        Future<Object> future3 = threadPool.submit(new Callable<Object>() {
-//            @Override
-//            public Object call() throws Exception {
-//                throw new RuntimeException();
-//            }
-//        });
-//
-//        System.out.println(future1.get());
-//        System.out.println(future2.get());
-//        System.out.println(future3.get());
-//    }
-
 }
