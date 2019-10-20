05935e09f0da46cde8dcae2a92afac31afa911c5
pimithie <xq262762708@aliyun.com>
2019-10-20 22:18:46 +0800

    CountDownLatch version

--- src/main/java/com/github/hcsp/multithread/WordCount.java
+++ src/main/java/com/github/hcsp/multithread/WordCount.java
@@ -1,18 +1,15 @@
 package com.github.hcsp.multithread;
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.Callable;
 
 public class WordCount {
 
@@ -20,37 +17,32 @@ public class WordCount {
 
     private final ExecutorService executor;
 
+    private CountDownLatch countDownLatch;
+
+    private final Object lock = new Object();
+
+    private final Map<String, Integer> result = new HashMap<>();
+
     public WordCount(int threadNum) {
         nThread = threadNum;
         executor = Executors.newFixedThreadPool(nThread);
     }
 
     // 统计文件中各单词的数量
-    public Map<String, Integer> count(List<File> files) throws InterruptedException, ExecutionException {
+    public Map<String, Integer> count(List<File> files) throws InterruptedException {
         // validate
         if (null == files || 0 == files.size()) {
             return new HashMap<>();
         }
-        final Map<String, Integer> res = new HashMap<>();
-        final List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+        countDownLatch = new CountDownLatch(files.size());
         for (File file : files) {
-            Future<Map<String, Integer>> future = executor.submit(new CountTask(file));
-            futures.add(future);
+            executor.submit(new CountTask(file));
         }
-        mergeResult(futures, res);
-        return res;
+        countDownLatch.await();
+        return result;
     }
 
-    private void mergeResult(List<Future<Map<String, Integer>>> futures, Map<String, Integer> res) throws ExecutionException, InterruptedException {
-        for (Future<Map<String, Integer>> future : futures) {
-            Map<String, Integer> temp = future.get();
-            for (Map.Entry<String, Integer> entry : temp.entrySet()) {
-                res.put(entry.getKey(), res.getOrDefault(entry.getKey(), 0) + entry.getValue());
-            }
-        }
-    }
-
-    class CountTask implements Callable<Map<String, Integer>> {
+    class CountTask implements Runnable {
 
         private final File file;
 
@@ -59,18 +51,32 @@ public class WordCount {
         }
 
         @Override
-        public Map<String, Integer> call() throws Exception {
-            final Map<String, Integer> map = new HashMap<>();
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+        public void run() {
+            BufferedReader bufferedReader = null;
+            try {
+                bufferedReader = new BufferedReader(new FileReader(file));
                 String line = null;
                 while ((line = bufferedReader.readLine()) != null) {
                     String[] words = line.split(" ");
                     for (String word : words) {
-                        map.put(word, map.getOrDefault(word, 0) + 1);
+                        synchronized (lock) {
+                            // 组合操作并不是线程安全的
+                            result.put(word, result.getOrDefault(word, 0) + 1);
+                        }
                     }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (null != bufferedReader) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                countDownLatch.countDown();
             }
-            return map;
         }
     }
 
