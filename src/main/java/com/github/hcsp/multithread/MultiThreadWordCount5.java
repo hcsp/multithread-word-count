ae594f4b0346f12de0307b0304da1faa8d88c2a9
yanghuadong <huadongyang@chutianyun.gov.cn>
2019-11-20 09:03:11 +0800

    '改一下格式'

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
@@ -11,15 +11,17 @@ import java.util.Map;
 public class MultiThreadWordCount5 {
     //     使用threadNum个线程，并发统计文件中各单词的数量
     public static Map<String, Integer> count(int threadNum, List<File> files) {
-      /*
-        为什么这种写法可以通过测试，而且预期结果看起来是正确的。但看起来并不是线程安全的，我也没有进行同步操作
+        /*
+       // 为什么这种写法可以通过测试，而且预期结果看起来是正确的。但看起来并不是线程安全的，我也没有进行同步操作
         Map<String, Integer> finalResult = new HashMap<>();
-        files.forEach(file -> {
+        files.parallelStream()
+            .forEach(file -> {
                 Map<String, Integer> wordCount = countSingleFile(file);
                 merge(finalResult, wordCount);
             });
          return finalResult;
          */
+
 
         return files.parallelStream()
                 .map(MultiThreadWordCount1::countSingleFile)
