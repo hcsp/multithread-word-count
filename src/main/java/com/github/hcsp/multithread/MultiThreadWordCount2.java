4c47993b8f858318c53768302ee1b5c472325e13
kisslonglin <1253461277@qq.com>
2019-12-30 14:10:34 +0800

    多种方式实现wordCount 1.CountDownLatch 2.Future搭配线程池 3.CyclicBarrier 4.Fork、Join框架 5.Lock

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,48 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+
+
 public class MultiThreadWordCount2 {
-    // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    //     使用threadNum个线程，并发统计文件中各单词的数量
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        Map<String, Integer> wordCounts = new HashMap<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(threadNum);
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+        for (File file : files) {
+            Future<Map<String, Integer>> future = executorService.submit(new Callable<Map<String, Integer>>() {
+                @Override
+                public Map<String, Integer> call() throws Exception {
+                    HashMap<String, Integer> wordCounts = new HashMap<>();
+                    List<String> allLines = Files.readAllLines(Paths.get(file.getPath()));
+                    for (String words : allLines) {
+                        String[] wordspaces = words.split("\\s");
+                        for (String word : wordspaces) {
+                            Integer orDefault = wordCounts.getOrDefault(word, 0) + 1;
+                            wordCounts.put(word, orDefault);
+                        }
+                    }
+                    return wordCounts;
+                }
+            });
+            futures.add(future);
+        }
+        for (Future<Map<String, Integer>> future : futures) {
+            try {
+                Map<String, Integer> futurewordCounts = future.get();
+                futurewordCounts.forEach((key, value) -> wordCounts.put(key, wordCounts.getOrDefault(key, 0) + value));
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return wordCounts;
+    }
 }
