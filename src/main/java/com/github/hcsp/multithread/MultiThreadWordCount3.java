fe6b760e6167f724cf2b666b1f5c55c86464a0d8
Maricaya <43136257+Maricaya@users.noreply.github.com>
2020-01-03 09:36:28 +0800

    完成wordCount (#62)
    完成wordCount (#62)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount3.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount3.java
@@ -1,8 +1,44 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import static com.github.hcsp.multithread.MultiThreadWordCount1.countSingleFile;
+import static com.github.hcsp.multithread.MultiThreadWordCount1.mergeMap;
+
 public class MultiThreadWordCount3 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+
+    private static CountDownLatch done;
+
+    //  使用threadNum个线程，并发统计文件中各单词的数量
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws InterruptedException, RuntimeException {
+        done = new CountDownLatch(files.size());
+        ExecutorService service = Executors.newFixedThreadPool(threadNum);
+        Map<String, Integer> result = new HashMap<>();
+
+        for (File file : files) {
+            service.submit(() -> {
+                Map<String, Integer> temp = null;
+                try {
+                    temp = countSingleFile(file);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                synchronized (MultiThreadWordCount2.class) {
+                    mergeMap(result, temp);
+                }
+                done.countDown();
+            });
+        }
+        done.await();
+        service.shutdown();
+
+        return result;
+    }
 }
