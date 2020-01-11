c37be849a442ac43abb0e0de1891f29a6d93563e
NervousOrange <zch693922442@outlook.com>
2020-01-11 14:27:19 +0100

    from NervousOrange: Assignment 3 of MultiThread. 2rd time

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -8,59 +8,42 @@ import java.util.List;
 import java.util.concurrent.*;
 
 public class MultiThreadWordCount1 {
-    // 使用threadNum个线程，并发统计文件中各单词的数量
-    public HashMap<String, Integer> count(int threadNum, List<File> files)  {
+
+    // 每个文件开一个线程，统计词频
+    public static HashMap<String, Integer> count(int threadNum, List<File> files) throws ExecutionException, InterruptedException {
         ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
         List<Future<HashMap<String, Integer>>> futures = new ArrayList<>();
         for (File file : files) {
-            BufferedReader fileReader = null;
-            try {
-                fileReader = new BufferedReader(new FileReader(file));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            futures.addAll(wordsCountSingleFile(fileReader, threadPool, threadNum));
+            futures.add(threadPool.submit(new WordsCountInFile(file)));
         }
         return resultMerge(futures);
     }
 
-    // 使用多线程对单文件进行词频记录工作，并将结果添加至类静态变量 wordsCountResultList
-    public static List<Future<HashMap<String, Integer>>> wordsCountSingleFile (BufferedReader fileReader, ExecutorService threadPool, int threadNum) {
-        List<Future<HashMap<String, Integer>>> countResultListSingleFile = new ArrayList<>();
-        for (int i = 0; i < threadNum; i++) {
-            countResultListSingleFile.add(threadPool.submit(new wordsCountInFile(fileReader)));
-        }
-        return countResultListSingleFile;
-    }
-
-    public static HashMap<String, Integer> resultMerge(List<Future<HashMap<String, Integer>>> wordsCountResultInList) {
-        for (Future<HashMap<String, Integer>> individualWordsCount : wordsCountResultInList) {
-            try {
-                return resultMergeHelper(individualWordsCount.get());
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
-    }
-
-    private static HashMap<String, Integer> resultMergeHelper(HashMap<String, Integer> stringIntegerConcurrentHashMap) {
+    public static HashMap<String, Integer> resultMerge(List<Future<HashMap<String, Integer>>> futures) throws ExecutionException, InterruptedException {
         HashMap<String, Integer> countResult = new HashMap<>();
-        for (String key : stringIntegerConcurrentHashMap.keySet()) {
-            countResult.put(key, stringIntegerConcurrentHashMap.getOrDefault(key, 0) + stringIntegerConcurrentHashMap.get(key));
+        for (Future<HashMap<String, Integer>> future : futures) {
+            resultMergeHelper(future.get(), countResult);
         }
         return countResult;
     }
 
-    static class wordsCountInFile implements Callable<HashMap<String, Integer>> {
+    private static HashMap<String, Integer> resultMergeHelper(HashMap<String, Integer> futureGet, HashMap<String, Integer> countResult) {
+        for (String key : futureGet.keySet()) {
+            countResult.put(key, (countResult.getOrDefault(key, 0) + futureGet.get(key)));
+        }
+        return countResult;
+    }
 
-        BufferedReader fileReader;
-        wordsCountInFile(BufferedReader fileReader) {
-            this.fileReader = fileReader;
+    static class WordsCountInFile implements Callable<HashMap<String, Integer>> {
+        File file;
+
+        WordsCountInFile(File file) {
+            this.file = file;
         }
 
         @Override
-        public HashMap<String, Integer> call() {
+        public HashMap<String, Integer> call() throws FileNotFoundException {
+            BufferedReader fileReader = new BufferedReader(new FileReader(file));
             HashMap<String, Integer> wordsCountInLine = new HashMap<>();
             try {
                 String stringInLine;
@@ -70,7 +53,7 @@ public class MultiThreadWordCount1 {
                         wordsCountInLine.put(string, wordsCountInLine.getOrDefault(string, 0) + 1);
                     }
                 }
-            }catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             return wordsCountInLine;
