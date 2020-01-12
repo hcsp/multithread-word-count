e9b4b87b09b72a0d11a4baed04dbc530a99cb9f0
丁胜 <dingduoduo0916@gmail.com>
2020-01-12 16:06:42 +0800

    多种方式实现WordCount (#67)
    多种方式实现WordCount (#67)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
@@ -1,8 +1,23 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class MultiThreadWordCount5 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        List<Map<String, Integer>> mapList = new ArrayList<>();
+        files.parallelStream().forEach(file -> {
+            try {
+                mapList.add(MultiThreadWordCount1.countOneFile(file));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return MultiThreadWordCount1.merge(mapList);
+    }
 }
