ab307d1a806f5c3c00622347754b3497f7b29b60
丁胜 <dingduoduo0916@gmail.com>
2020-01-15 21:19:05 +0800

    Dsying1 (#72)
    Dsying1 (#72)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount4.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount4.java
@@ -1,8 +1,28 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
 public class MultiThreadWordCount4 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+        public static Map<String, Integer> count(int threadNum, List<File> files) {
+            ExecutorService threadPool = Executors.newFixedThreadPool(files.size());
+            List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+            files.forEach(file -> {
+                futures.add(threadPool.submit(() -> MultiThreadWordCount1.countOneFile(file)));
+            });
+
+            List<Map<String, Integer>> mapList = futures.stream().map(mapFuture -> {
+                try {
+                    return mapFuture.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            }).collect(Collectors.toList());
+            return MultiThreadWordCount1.merge(mapList);
+        }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount6.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount6.java
@@ -2,28 +2,34 @@ package com.github.hcsp.multithread;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 
-public class MultiThreadWordCount3 {
+public class MultiThreadWordCount6 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
     public static Map<String, Integer> count(int threadNum, List<File> files) throws InterruptedException {
+        // 使用许可数量为1 的信号量作为 互斥锁
+        Semaphore lock = new Semaphore(1);
+        // 创建 闭锁用于 所有文件被处理后 主线程 执行合并操作
         CountDownLatch latch = new CountDownLatch(files.size());
-        List<Map<String, Integer>> mapList = new CopyOnWriteArrayList<>();
+
+        List<Map<String, Integer>> mapList = new ArrayList<>();
         files.forEach(file -> {
             new Thread(() -> {
                 try {
+                    lock.acquire();
                     mapList.add(MultiThreadWordCount1.countOneFile(file));
-                } catch (IOException e) {
-                    e.printStackTrace();
+                } catch (InterruptedException | IOException e) {
+                    throw new RuntimeException(e);
                 } finally {
                     latch.countDown();
+                    lock.release();
                 }
             }).start();
         });
-
         latch.await();
         return MultiThreadWordCount1.merge(mapList);
     }
