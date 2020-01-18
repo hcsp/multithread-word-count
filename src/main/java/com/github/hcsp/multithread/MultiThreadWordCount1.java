67413f01753d6b176577342e43e1c7889e48d1ab
xdc <1434412784@qq.com>
2020-01-18 22:55:10 +0800

    初次尝试

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,26 @@
 package com.github.hcsp.multithread;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws FileNotFoundException, ExecutionException, InterruptedException {
+        Map<String, Integer> finalResult = new HashMap<>();
+
+        for (File file : files) {
+            WordCount fileWordCount = new WordCount(threadNum);
+            Map<String, Integer> resultFromWordCount = fileWordCount.count(file);
+
+            WordCount.mergeWorkerResultIntoFinalResult(resultFromWordCount, finalResult);
+        }
+
+        return finalResult;
     }
 }
