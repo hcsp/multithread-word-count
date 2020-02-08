8cda7bbd5b5d63b6bba05c0fdb24f8bf7e1cb3c6
Felix <fiymi666@gmail.com>
2020-02-08 20:10:53 +0800

    修改伪多线程方法

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.multithread;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,10 +9,11 @@ import java.util.concurrent.*;
 
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-
     public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException, ExecutionException, InterruptedException {
         ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
         Map<String, Integer> resultMap = new HashMap<>();
+        //Future<Map<String, Integer>> future = null;
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
         for (File file : files) {
             Reader reader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(reader);
@@ -30,12 +32,16 @@ public class MultiThreadWordCount1 {
                 }
             }
             Future<Map<String, Integer>> future = threadPool.submit(new task());
-
+            futures.add(future);
+        }
+        for (Future<Map<String, Integer>> future : futures) {
             for (String word : future.get().keySet()) {
                 int mergedMapWordNum = future.get().get(word);
                 resultMap.put(word, resultMap.getOrDefault(word, 0) + mergedMapWordNum);
             }
         }
+
+
         return resultMap;
     }
 }
