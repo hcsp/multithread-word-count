99a240d2bd5e6d669c694342054ce71787655680
benectic <xuanzebin@126.com>
2020-02-15 18:05:44 +0800

    feat: multithread word count

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -6,20 +6,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class MultiThreadWordCount1 {
-    private static AtomicInteger fileIndex = new AtomicInteger(0);
     private static List<Future<HashMap<String, Integer>>> futures = new ArrayList<>();
-    private static ConcurrentHashMap<Integer, BufferedReader> commonReader = new ConcurrentHashMap<>();
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) throws ExecutionException, InterruptedException {
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws FileNotFoundException, ExecutionException, InterruptedException {
         ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
-        HashMap<String, Integer> finalResult = new HashMap<>();
-        for (int i = 0; i < threadNum; i++) {
-            Future<HashMap<String, Integer>> future = threadPool.submit(() -> getTheCountResult(files));
-            futures.add(future);
+        for (File file : files) {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            for (int i = 0; i < threadNum; i++) {
+                Future<HashMap<String, Integer>> future = threadPool.submit(() -> getTheCountResult(file, reader));
+                futures.add(future);
+            }
         }
+
+        return mergeTheFutureAndGetTheResult(futures);
+    }
+
+    private static HashMap<String, Integer> mergeTheFutureAndGetTheResult(List<Future<HashMap<String, Integer>>> futures) throws ExecutionException, InterruptedException {
+        HashMap<String, Integer> finalResult = new HashMap<>();
         for (Future<HashMap<String, Integer>> future:futures) {
             for (Map.Entry<String, Integer> entry: future.get().entrySet()) {
                 finalResult.put(entry.getKey(), finalResult.getOrDefault(entry.getKey(), 0) + entry.getValue());
@@ -28,19 +33,14 @@ public class MultiThreadWordCount1 {
         return finalResult;
     }
 
-    private static HashMap<String, Integer> getTheCountResult(List<File> files) throws IOException {
+    private static HashMap<String, Integer> getTheCountResult(File file, BufferedReader reader) throws IOException {
         HashMap<String, Integer> result = new HashMap<>();
-        while (files.size() > fileIndex.get()) {
-            File file = files.get(fileIndex.get());
-            BufferedReader reader = commonReader.getOrDefault(fileIndex.get(), new BufferedReader(new FileReader(file)));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] words = line.split(" ");
-                for (String word : words) {
-                    result.put(word, result.getOrDefault(word, 0) + 1);
-                }
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                result.put(word, result.getOrDefault(word, 0) + 1);
             }
-            fileIndex.addAndGet(1);
         }
         return result;
     }
