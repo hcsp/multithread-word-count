fad751a29ce0c378731078a561303433364c8c90
Dygalo <59307187+dygalo@users.noreply.github.com>
2020-02-26 10:04:37 +0800

    Dygalo (#93)
    Dygalo (#93)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,44 @@
 package com.github.hcsp.multithread;
 
-import java.io.File;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class MultiThreadWordCount1 {
-    // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws FileNotFoundException, ExecutionException, InterruptedException {
+        Vector<InputStream> inputStreamVercotr = new Vector<>();
+        ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
+        for (File file : files) {
+            InputStream inputStream = new FileInputStream(file);
+            inputStreamVercotr.add(inputStream);
+        }
+        SequenceInputStream sequenceInputStream = new SequenceInputStream(inputStreamVercotr.elements());
+        Map<String, Integer> finalResult = new HashMap<>();
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(sequenceInputStream));
+        for (int i = 0; i < threadNum; i++) {
+            futures.add(threadPool.submit(() -> {
+                String line = null;
+                Map<String, Integer> result = new HashMap<>();
+                while ((line = reader.readLine()) != null) {
+                    String[] words = line.split(" ");
+                    for (String word : words) {
+                        {
+                            result.put(word, result.getOrDefault(word, 0) + 1);
+                        }
+                    }
+                }
+                return result;
+            }));
+        }
+        for (Future<Map<String, Integer>> map : futures) {
+            Map<String, Integer> map1 = map.get();
+            for (Map.Entry<String, Integer> entry : map1.entrySet()) {
+                int mergedResult = finalResult.getOrDefault(entry.getKey(), 0) + entry.getValue();
+                finalResult.put(entry.getKey(), mergedResult);
+            }
+        }
+        return finalResult;
     }
 }
