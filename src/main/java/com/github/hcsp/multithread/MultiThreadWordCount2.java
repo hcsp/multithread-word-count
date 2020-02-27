8863a3250f9aca20be14aa38838751beb7ba720e
Ir <1319138081@qq.com>
2020-02-27 13:00:57 +0800

    multithread-word-count completed (#94)
    multithread-word-count completed (#94)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,61 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+/**
+ * {@link java.util.concurrent.locks.Lock} / {@link java.util.concurrent.locks.Condition} implements
+ */
 public class MultiThreadWordCount2 {
+    static BlockingQueue<Map<String, Integer>> resultList = new LinkedBlockingQueue<>();
+    static BlockingQueue<File> fileBlockingQueue = null;
+    static Lock lock = new ReentrantLock();
+    static Condition condition = lock.newCondition();
+
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        fileBlockingQueue = new LinkedBlockingQueue<>(files);
+
+        IntStream.range(0, threadNum)
+                .forEach(i -> new CountThread().start());
+
+        lock.lock();
+        try {
+            while (resultList.size() < files.size()) {
+                condition.await();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+        return MultiThreadWordCount1.flat(resultList);
+    }
+
+    static class CountThread extends Thread {
+        @Override
+        public void run() {
+            lock.lock();
+            try {
+                while (!fileBlockingQueue.isEmpty()) {
+                    File file = fileBlockingQueue.poll();
+                    if (file == null) {
+                        break;
+                    }
+                    resultList.add(MultiThreadWordCount1.countOneFile(file));
+                }
+                condition.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
@@ -1,8 +1,48 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * {@link java.util.concurrent.ForkJoinPool} implements
+ */
 public class MultiThreadWordCount5 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws ExecutionException, InterruptedException {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.submit(new CountTask(files)).get();
+    }
+
+    static class CountTask extends RecursiveTask<Map<String, Integer>> {
+
+        List<File> fileList;
+
+        CountTask(List<File> fileList) {
+            this.fileList = fileList;
+        }
+
+        @Override
+        protected Map<String, Integer> compute() {
+            if (fileList.isEmpty()) {
+                return Collections.emptyMap();
+            }
+            Map<String, Integer> map1 = MultiThreadWordCount1.countOneFile(fileList.get(0));
+            Map<String, Integer> map2 = new CountTask(fileList.subList(1, fileList.size())).compute();
+            return merge(map1, map2);
+        }
+    }
+
+    public static Map<String, Integer> merge(Map<String, Integer> map1, Map<String, Integer> map2) {
+        Map<String, Integer> result = new HashMap<>();
+        Set<String> keys = new HashSet<>();
+        keys.addAll(map1.keySet());
+        keys.addAll(map2.keySet());
+        for (String key : keys) {
+            result.put(key, map1.getOrDefault(key, 0) + map2.getOrDefault(key, 0));
+        }
+        return result;
+    }
 }
