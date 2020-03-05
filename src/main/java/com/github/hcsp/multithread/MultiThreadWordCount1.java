155672ada923fd01d059f9451eca6e89b74a423f
Lee <lizhijie_lee@126.com>
2020-03-05 16:02:32 +0800

    初步提交

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,48 @@
 package com.github.hcsp.multithread;
 
-import java.io.File;
+import java.io.*;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MultiThreadWordCount1 {
+    private static ConcurrentHashMap<String, Integer> concurrentHashMap = new ConcurrentHashMap<>();
+
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws FileNotFoundException {
+        for (int i = 0; i < threadNum; i++) {
+            new Worker(files).start();
+        }
+        return concurrentHashMap;
+    }
+
+    static class Worker extends Thread {
+        private List<File> files;
+
+        Worker(List<File> files) {
+            this.files = files;
+        }
+
+        @Override
+        public void run() {
+            try {
+                readFiles(files);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void readFiles(List<File> files) throws IOException {
+            for (File file : files) {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] words = line.split(" ");
+                    for (String word : words) {
+                        concurrentHashMap.put(word, concurrentHashMap.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+        }
     }
 }
