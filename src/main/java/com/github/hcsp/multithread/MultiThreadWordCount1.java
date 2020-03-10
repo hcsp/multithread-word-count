3eb8d6c899e059d241512a09be58b6eae5f41c9d
TangsongyuanmingQingHad <had13625428701@aliyun.com>
2020-03-11 00:20:35 +0800

    未完待续

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -8,7 +8,6 @@ public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
     public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException, ExecutionException, InterruptedException {
         ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
-        List<Map<String, Integer>> list = new ArrayList<>();
         HashMap<String, Integer> resultMap = new HashMap<>();
 
         for (File file : files) {
