80c6be14774b5cc42826bbf6a76c6abc88852178
huiquan99 <53985556+huiquan99@users.noreply.github.com>
2020-03-18 10:36:56 +0800

    help QAQ (#101)
    help QAQ (#101)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,91 @@
 package com.github.hcsp.multithread;
 
-import java.io.File;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    private final int threadNum;
+
+    public MultiThreadWordCount1(int threadNum) {
+        this.threadNum = threadNum;
     }
+
+    public int getThreadNum() {
+        return threadNum;
+    }
+
+
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws FileNotFoundException, ExecutionException, InterruptedException {
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
+
+        List<Map<String, Integer>> resultList = new ArrayList<>();
+        Map<String, Integer> lastMap = new HashMap<>();
+
+
+        for (File file : files) {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            Map<String, Integer> resultMap = new HashMap<>();
+            List<Future<Map<String, Integer>>> list = new ArrayList<>();
+            for (int i = 0; i < threadNum; i++) {
+                list.add(threadPool.submit(new Callable<Map<String, Integer>>() {
+                    @Override
+                    public Map<String, Integer> call() throws Exception {
+                        String line;
+                        Map<String, Integer> result = new HashMap<>();
+                        while ((line = reader.readLine()) != null) {
+                            String[] words = line.split(" ");
+
+                            for (String word : words) {
+                                result.put(word, result.getOrDefault(word, 0) + 1);
+                            }
+                        }
+                        return result;
+                    }
+                }));
+            }
+
+            //此处得到了Future的集合List，要将它们合并
+            for (Future<Map<String, Integer>> mapFuture : list) {
+                //得到每个结果中的Map
+                Map<String, Integer> stringIntegerMap = mapFuture.get();
+                Set<Map.Entry<String, Integer>> entries = stringIntegerMap.entrySet();
+
+                for (Map.Entry<String, Integer> entry : entries) {
+                    String word = entry.getKey();
+
+                    int mergeValue = entry.getValue() + resultMap.getOrDefault(word, 0);
+                    //一个set一个set构成本文件中的map
+                    resultMap.put(word, mergeValue);
+
+                }
+
+            }
+            //至此，一个文件中的Map正式处理完，加入结果要处理的list中
+            resultList.add(resultMap);
+        }
+
+        //对list中的各个文件的Map进行合并
+
+        for (Map<String, Integer> eachMap : resultList) {
+            Set<Map.Entry<String, Integer>> entries = eachMap.entrySet();
+            for (Map.Entry<String, Integer> entry : entries) {
+                String word = entry.getKey();
+
+                int mergeValue = entry.getValue() + lastMap.getOrDefault(word, 0);
+                //一个set一个set构成本文件中的map
+                lastMap.put(word, mergeValue);
+            }
+        }
+
+
+        //返回最终返回语句
+        threadPool.shutdown();
+        return lastMap;
+
+    }
+
+
 }
