e5c6ec3b1a8a11b06cb7e69cfdbd573305745f02
硬核机器人 <49554844+hcsp-bot@users.noreply.github.com>
2020-04-16 16:13:30 +0800

    多线程wordcount练习 (#120)
    多线程wordcount练习 (#120)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,78 @@
 package com.github.hcsp.multithread;
 
+import com.google.common.collect.Lists;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 public class MultiThreadWordCount1 {
+    static CountDownLatch latch = null;
     // 使用threadNum个线程，并发统计文件中各单词的数量
     public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+        Map<String, Integer> result = new HashMap<>();
+        List<Map<String, Integer>> wordCountMapList = new ArrayList<>();
+        latch = new CountDownLatch(threadNum);
+        List<List<File>> fileGroup = Lists.partition(files, threadNum);
+        for (List<File> fileList : fileGroup) {
+            new Thread(new ReadFileWithCountLatch(fileList, latch, wordCountMapList)).start();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (Map<String, Integer> wordCountMap : wordCountMapList) {
+            ReadFileUtils.mergeMap(result, wordCountMap);
+        }
+        return result;
     }
+
+    public static class ReadFileWithCountLatch implements Runnable {
+        private CountDownLatch latch;
+        private Map<String, Integer> map = new HashMap<>();
+        private List<Map<String, Integer>> maps;
+        private List<File> files;
+
+        public List<Map<String, Integer>> getMaps() {
+            return maps;
+        }
+
+        public ReadFileWithCountLatch(List<File> files, CountDownLatch latch,
+                                      List<Map<String, Integer>> mapList) {
+            this.files = files;
+            this.latch = latch;
+            maps = mapList;
+        }
+
+        @Override
+        public void run() {
+            for (File file : files) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    ReadFileUtils.readWordsToMap(map, reader);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException("file not found: " + file.getName());
+                } catch (IOException e) {
+                    throw new RuntimeException(e.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            }
+            maps.add(map);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count(10, new ReadFileUtils().createTestFiles()));
+    }
+
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,41 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MultiThreadWordCount2 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        Map<String, Integer> result = new HashMap<>();
+        List<Map<String, Integer>> mapList = new ArrayList<>();
+
+        List<ReadFileWithThread> threads = new ArrayList<>();
+
+        for (File file : files) {
+            ReadFileWithThread readFileThread = new ReadFileWithThread(file);
+            threads.add(readFileThread);
+            readFileThread.start();
+        }
+
+        for (ReadFileWithThread thread : threads) {
+            try {
+                thread.join();
+                mapList.add(thread.getMap());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (Map<String, Integer> source : mapList) {
+            ReadFileUtils.mergeMap(result, source);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count(10, new ReadFileUtils().createTestFiles()));
+    }
 }
