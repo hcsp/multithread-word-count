425fccfa304854093ad83fcc2065de811cf37062
Sunpeng526 <215860865@qq.com>
2020-05-08 02:26:58 -0700

    test

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,60 +1,55 @@
 package com.github.hcsp.multithread;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量 Lock/Condition
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        List<File> files = Arrays.asList(new File("c:/users/sunp/git/multithread-word-count/target/test3.txt"),
-                new File("c:/users/sunp/git/multithread-word-count/target/test4.txt"));
-        System.out.println(count(10, files));
+        List<File> files = Arrays.asList(new File("C:/Users/sunp/git/test-files/test1.txt"),
+                new File("C:/Users/sunp/git/test-files/test2.txt"),
+                new File("C:/Users/sunp/git/test-files/test3.txt"),
+                new File("C:/Users/sunp/git/test-files/test4.txt"));
+        System.out.println(count(4, files));
     }
 
     public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException, InterruptedException {
         final Object lock = new Object();
-        int count = 0;
+        AtomicInteger chucksCount = new AtomicInteger(0);
+        List<List<File>> fileChucks = MultiThreadWordUtility.fileSplit(threadNum, files);
         Map<String, Integer> result = new HashMap<>();
         List<Map<String, Integer>> middleList = new ArrayList<>();
-        for (File file : files) {
-            int newCount = count;
-            BufferedReader bReader = Files.newBufferedReader(file.toPath());
-            for (int i = 0; i < threadNum; i++) {
-                Map<String, Integer> middleResult = new HashMap<>();
-                Thread thread = new Thread(() -> {
-                    try {
-                        String line;
-                        while (null != (line = bReader.readLine())) {
-                            String[] strings = line.split(" ");
-                            for (String word : strings) {
-                                middleResult.put(word, middleResult.getOrDefault(word, 0) + 1);
-                            }
-                        }
-                        middleList.add(middleResult);
-                        synchronized (lock) {
-                            if (newCount == files.size()) {
-                                lock.notify();
-                            }
-                        }
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+        for (List<File> fileChuck : fileChucks) {
+            new Thread(() -> {
+                synchronized (lock) {
+                    while (chucksCount.get() == fileChucks.size()) {
+                        lock.notify();
                     }
-                });
-                thread.start();            
-            }
-            count++;
+                    try {                       
+                        Map<String, Integer> middleResult = MultiThreadWordUtility.fileChuckCount(fileChuck);
+                        middleList.add(middleResult);
+                        chucksCount.addAndGet(1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }                  
+                }
+            }).start();
         }
+
         synchronized (lock) {
-            if (count != files.size()) {
-                lock.wait();
+            while (chucksCount.get() != fileChucks.size()) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
             for (Map<String, Integer> stringIntegerMap : middleList) {
                 stringIntegerMap.forEach((key, value) -> {
