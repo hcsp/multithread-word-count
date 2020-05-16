701b2e9ee2bfd92d6b762d80fd095f86ce8256c1
PudgeWL <nbhaohao@gmail.com>
2020-05-16 09:53:02 +0800

    feat: 完成挑战 (#135)
    feat: 完成挑战 (#135)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,51 @@
 package com.github.hcsp.multithread;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.*;
 
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
     public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadNum);
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+        for (File file : files) {
+            Future<Map<String, Integer>> futureFileContent = readFileWithThreadPool(file, executorService);
+            futures.add(futureFileContent);
+        }
+        return parseFutureFileContents(futures);
+    }
+
+    private static Map<String, Integer> parseFutureFileContents(List<Future<Map<String, Integer>>> futures) {
+        List<Map<String, Integer>> mapList = new ArrayList<>();
+        for (Future<Map<String, Integer>> future : futures) {
+            try {
+                Map<String, Integer> singleFileResult = future.get();
+                mapList.add(singleFileResult);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        return WordCountUtils.mergeMap(mapList);
+    }
+
+    public static Future<Map<String, Integer>> readFileWithThreadPool(File file, ExecutorService executorService) {
+        return executorService.submit(new ReadFileTask(file));
+    }
+
+    static class ReadFileTask implements Callable<Map<String, Integer>> {
+        File file;
+
+        ReadFileTask(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public Map<String, Integer> call() throws IOException {
+            return WordCountUtils.statisticsFileWordCount(file);
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,70 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class MultiThreadWordCount2 {
+
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        final Object lock = new Object();
+        List<Map<String, Integer>> resultList = new ArrayList<>();
+        BlockingQueue<File> todoFilesQueue = getFilesQueue(files);
+        for (int i = 0; i < threadNum; i++) {
+            new ReadFileTask(resultList, todoFilesQueue, lock).start();
+        }
+        synchronized (lock) {
+            while (!isTasksComplete(resultList, files)) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return WordCountUtils.mergeMap(resultList);
+        }
+    }
+
+    public static boolean isTasksComplete(List<Map<String, Integer>> results, List<File> files) {
+        return results.size() == files.size();
+    }
+
+    public static BlockingQueue<File> getFilesQueue(List<File> files) {
+        return new ArrayBlockingQueue<>(files.size(), false, files);
+    }
+
+    static class ReadFileTask extends Thread {
+        private final List<Map<String, Integer>> resultList;
+        private final BlockingQueue<File> todoFilesQueue;
+        private final Object lock;
+
+        ReadFileTask(List<Map<String, Integer>> resultList, BlockingQueue<File> todoFilesQueue, Object lock) {
+            this.resultList = resultList;
+            this.todoFilesQueue = todoFilesQueue;
+            this.lock = lock;
+        }
+
+        @Override
+        public void run() {
+            File file;
+            while ((file = todoFilesQueue.poll()) != null) {
+                try {
+                    Map<String, Integer> oneFileResult = WordCountUtils.statisticsFileWordCount(file);
+                    synchronized (lock) {
+                        resultList.add(oneFileResult);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            synchronized (lock) {
+                lock.notify();
+            }
+        }
+    }
 }
