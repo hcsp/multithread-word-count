f4d5c82bb8f22c08dbb282e575595b274ad68bb0
Ryan <49991585+Ryan-Y-C@users.noreply.github.com>
2020-05-25 22:55:36 +0800

    多种方式实现Word Count程序。 (#136)
    多种方式实现Word Count程序。 (#136)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
@@ -1,8 +1,34 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+
 public class MultiThreadWordCount5 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+
+    public static Map<String, Integer> count(List<File> files) {
+        Map<String, Integer> wordCount = new ConcurrentHashMap<>();
+
+        CountDownLatch latch = new CountDownLatch(files.size());
+        List<Map<String, Integer>> wordCountList = new ArrayList<>();
+        for (File file : files) {
+            new Thread(() -> {
+                wordCountList.add(new WordCountTask().task(file));
+                latch.countDown();
+            }).start();
+        }
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        for (Map<String, Integer> resultFromWorker : wordCountList) {
+            new MergeWorker().mergeWorkerResultIntoFinalResult(resultFromWorker, wordCount);
+        }
+        return wordCount;
+    }
 }
