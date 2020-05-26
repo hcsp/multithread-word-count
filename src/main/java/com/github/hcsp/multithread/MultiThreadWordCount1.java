fbe73ac1facf805cab7efb55d8737414f0ccaf3e
SuyuZhuang <zhuangsuyu1993@163.com>
2020-05-26 23:24:34 +0800

    5种方法实现了word count

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,34 @@
 package com.github.hcsp.multithread;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
 
+/**
+ * CountDownLatch实现
+ */
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
     public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+        CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        List<Map<String, Integer>> countMapList = new CopyOnWriteArrayList<>();
+        Map<String, Integer> finalResult = new HashMap<>();
+        for (File file : files) {
+            new Thread(() -> {
+                Map<String, Integer> oneFileMap = Utils.countOneFile(file);
+                countMapList.add(oneFileMap);
+                countDownLatch.countDown();
+            }).start();
+        }
+        try {
+            countDownLatch.await();
+            finalResult = Utils.mergeMapList(countMapList);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return finalResult;
     }
 }
