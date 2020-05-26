04005af514b593645f3843165f7d638b12046520
Suyu <zhuangsuyu1993@163.com>
2020-05-26 23:36:44 +0800

    5种方式实现wordCount (#138)
    5种方式实现wordCount (#138)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,52 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Future与线程池
+ */
 public class MultiThreadWordCount2 {
-    // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    //使用threadNum个线程，并发统计文件中各单词的数量
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+        for (File file : files) {
+            Future<Map<String, Integer>> future = threadPool.submit(new WordCount(file));
+            futures.add(future);
+        }
+
+        Map<String, Integer> finalResult = new HashMap<>();
+        for (Future<Map<String, Integer>> future : futures) {
+            try {
+                Map<String, Integer> result = future.get();
+                Utils.mergeSourceMapToDest(finalResult, result);
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        threadPool.shutdown();
+        return finalResult;
+    }
+
+    static class WordCount implements Callable<Map<String, Integer>> {
+        File file;
+
+        WordCount(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public Map<String, Integer> call() throws Exception {
+            return Utils.countOneFile(file);
+        }
+    }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount3.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount3.java
@@ -1,8 +1,40 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * CyclicBarrier实现
+ */
 public class MultiThreadWordCount3 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        CyclicBarrier barrier = new CyclicBarrier(threadNum + 1);
+        Map<String, Integer> result = new HashMap<>();
+        List<Map<String, Integer>> mapList = new CopyOnWriteArrayList<>();
+        for (File file : files) {
+            new Thread(() -> {
+                Map<String, Integer> oneFileMap = Utils.countOneFile(file);
+                mapList.add(oneFileMap);
+                try {
+                    barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+        }
+
+        try {
+            barrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+        result = Utils.mergeMapList(mapList);
+        return result;
+    }
 }
