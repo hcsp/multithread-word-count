ac1b9686081e0bff29f06f1a3cf05075db620df0
luhao <linesahao@outlook.com>
2020-06-01 20:51:23 +0800

    多线程wordcount.

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,80 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.*;
 
+/**
+    Callable/Future，每个文件一个线程，互相不影响。
+ */
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
     public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+        long t0 = System.currentTimeMillis();
+        ExecutorService pool = Executors.newFixedThreadPool(threadNum);
+
+        List<Future<Map<String, Integer>>> futureList = new ArrayList<>();
+
+        for (File file : files) {
+            Future<Map<String, Integer>> mapFuture = pool.submit(new FileWordCount(file));
+            futureList.add(mapFuture);
+        }
+
+        Map<String, Integer> finallyResult = new HashMap<>();
+
+        for (Future<Map<String, Integer>> future : futureList) {
+            try {
+                Map<String, Integer> wordCountMap = future.get();
+                eachMapToFinallyResult(wordCountMap, finallyResult);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        long t1 = System.currentTimeMillis();
+        System.out.println("MultiThreadWordCount1 cost " + (t1 - t0) + " ms");
+        return finallyResult;
+    }
+
+    static void eachMapToFinallyResult(Map<String, Integer> map, Map<String, Integer> finallyResult) {
+        for (Map.Entry<String, Integer> wordEntry : map.entrySet()) {
+            finallyResult.put(wordEntry.getKey(), finallyResult.getOrDefault(wordEntry.getKey(), 0)
+                    +
+                    wordEntry.getValue());
+        }
+    }
+
+    // 线程执行体，计算单个文件的wordCount
+    static class FileWordCount implements Callable<Map<String, Integer>> {
+        private final File file;
+        private final Map<String, Integer> map = new HashMap<>();
+
+        FileWordCount(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public Map<String, Integer> call() throws Exception {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    lineWordCount(line, map);
+                }
+            }
+            return map;
+        }
+
+        private void lineWordCount(String line, Map<String, Integer> map) {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                map.put(word, map.getOrDefault(word, 0) + 1);
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,71 @@
 package com.github.hcsp.multithread;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+    多个线程同时写入同一个Map来完成单词统计。
+ */
 public class MultiThreadWordCount2 {
+    private static final Object lock = new Object();
+
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        long t0 = System.currentTimeMillis();
+        ExecutorService pool = Executors.newFixedThreadPool(threadNum);
+
+        Map<String, Integer> finallyResult = new HashMap<>();
+
+        for (File file : files) {
+            pool.submit(new MultiThreadWordCount2.FileWordCount(file, lock, finallyResult));
+        }
+        pool.shutdown();
+        try {
+            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("word count执行线程没有顺利结束", e);
+        }
+        long t1 = System.currentTimeMillis();
+        System.out.println("MultiThreadWordCount2 cost " + (t1 - t0) + " ms");
+        return finallyResult;
+    }
+
+    // 线程执行体，计算单个文件的wordCount
+    static class FileWordCount implements Runnable {
+        private final File file;
+        private final Object lock;
+        private final Map<String, Integer> finallyResult;
+
+        FileWordCount(File file, Object lock, Map<String, Integer> finallyResult) {
+            this.file = file;
+            this.lock = lock;
+            this.finallyResult = finallyResult;
+        }
+
+        @Override
+        public void run() {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    lineWordCount(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void lineWordCount(String line) {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                synchronized (lock) {
+                    finallyResult.put(word, finallyResult.getOrDefault(word, 0) + 1);
+                }
+            }
+        }
+
+
+    }
 }
