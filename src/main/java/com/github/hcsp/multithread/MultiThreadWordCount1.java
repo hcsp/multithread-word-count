85180a71c43f18194410ba453eae971bbb66edf9
McDul <McDul19960103@gmail.com>
2020-06-29 17:35:58 +0800

    统计每个小写字母出现的次数

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,58 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 public class MultiThreadWordCount1 {
-    // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    private static ConcurrentHashMap<String, Integer> filesResult = new ConcurrentHashMap<String, Integer>();
+
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException {
+        for (File file : files) {
+            BufferedReader reader = Files.newBufferedReader(file.toPath());
+            createThreadPools(threadNum, reader);
+        }
+        return filesResult;
+    }
+
+    private static void createThreadPools(int threadNum, BufferedReader reader) {
+        for (int i = 0; i < threadNum; i++) {
+            CountWords countWords = new CountWords(filesResult, reader);
+            countWords.run();
+        }
+    }
+}
+
+class CountWords implements Runnable {
+    ConcurrentHashMap<String, Integer> filesResult;
+    BufferedReader reader;
+
+    CountWords(ConcurrentHashMap<String, Integer> filesResult, BufferedReader reader) {
+        this.filesResult = filesResult;
+        this.reader = reader;
+    }
+
+    @Override
+    public void run() {
+        String line;
+        while (true) {
+            try {
+                if ((line = reader.readLine()) == null) {
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+            String[] words = line.split(" ");
+            for (String word : words) {
+                filesResult.put(word, filesResult.getOrDefault(word, 0) + 1);
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,61 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class MultiThreadWordCount2 {
+    private static ConcurrentHashMap<String, Integer> result = new ConcurrentHashMap<>();
+
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws FileNotFoundException {
+        for (File file : files) {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            readOneFileAndCountWord(reader, threadNum);
+        }
+        return result;
+    }
+
+    private static void readOneFileAndCountWord(BufferedReader reader, int threadNum) {
+        for (int i = 0; i < threadNum; i++) {
+            CountWord countWord = new CountWord(reader, result);
+            countWord.call();
+        }
+    }
+
+    private static class CountWord implements Callable<Map<String, Integer>> {
+        BufferedReader reader;
+        Map<String, Integer> result;
+
+        public CountWord(BufferedReader reader, ConcurrentHashMap<String, Integer> result) {
+            this.reader = reader;
+            this.result = result;
+        }
+
+        @Override
+        public Map<String, Integer> call() {
+            String line;
+            while (true) {
+                try {
+                    if ((line = reader.readLine()) == null) {
+                        break;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    result.put(word, result.getOrDefault(word, 0) + 1);
+                }
+            }
+            return result;
+        }
+    }
 }
