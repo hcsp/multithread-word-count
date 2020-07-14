e080c43ef1409423a5dfca1a2ca4402bb70ab0f8
chenxiaolani <2524729719@qq.com>
2020-07-15 00:14:46 +0800

    使用threadPool统计单词个数

---
+++ src/main/java/com/github/hcsp/multithread/Utils.java
@@ -0,0 +1,54 @@
+package com.github.hcsp.multithread;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Utils {
+
+    /**
+     * 合并map
+     */
+    public static Map<String, Integer> mergeMap(Map<String, Integer> targetMap, Map<String, Integer> finalResult) {
+        for (Map.Entry<String, Integer> entry : targetMap.entrySet()) {
+            finalResult.put(entry.getKey(), finalResult.getOrDefault(entry.getKey(), 0) + entry.getValue());
+        }
+        return finalResult;
+    }
+
+    /**
+     * 合并list与map
+     */
+    public static Map<String, Integer> mergeMapList(List<Map<String, Integer>> mapList) {
+        Map<String, Integer> result = new HashMap<>();
+        for (Map<String, Integer> oneMap : mapList) {
+            for (Map.Entry<String, Integer> entry : oneMap.entrySet()) {
+                result.put(entry.getKey(), result.getOrDefault(entry.getKey(), 0) + entry.getValue());
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 统计一个文件的单词个数
+     */
+    public static Map<String, Integer> countFile(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        Map<String, Integer> countResult = new HashMap<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] wordsArr = line.split(" ");
+            for (String word : wordsArr) {
+                countResult.put(word, countResult.getOrDefault(word, 0) + 1);
+            }
+        }
+        return countResult;
+    }
+
+}
+
