57fa7e58aa56ac82aaa916c00af27b883500dd09
walkerxin <44486634+walkerxin@users.noreply.github.com>
2020-08-01 01:28:01 +0800

    use multithread to count words in file (#161)
    use multithread to count words in file (#161)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,92 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MultiThreadWordCount1 {
+    private static Map<String, Integer> finalMap = new LinkedHashMap<>();
+    private static final Object LOCK = new Object();
+    private static boolean finishRead = false;
+
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException, InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
+        List<BufferedReader> readers = getReaders(files);
+
+        for (int i = 0; i < threadNum; i++) {
+            threadPool.execute(new CountWord(readers));
+        }
+
+        synchronized (LOCK) {
+            while (!finishRead) {
+                LOCK.wait();
+            }
+        }
+
+        threadPool.shutdown();
+        closeReaders(readers);
+        return finalMap;
+    }
+
+    private static List<BufferedReader> getReaders(List<File> files) throws FileNotFoundException {
+        List<BufferedReader> readers = new ArrayList<>(files.size());
+        for (File file : files) {
+            readers.add(new BufferedReader(new FileReader(file)));
+        }
+        return readers;
+    }
+
+    public static class CountWord extends Thread {
+        private List<BufferedReader> readers;
+
+        public CountWord(List<BufferedReader> readers) {
+            this.readers = readers;
+        }
+
+        @Override
+        public void run() {
+            synchronized (LOCK) {
+                String s;
+                while (!finishRead && (s = getReadLine(readers)) != null) {
+                    for (String word : s.split(" ")) {
+                        if (!"".equals(word)) {
+                            finalMap.put(word, finalMap.getOrDefault(word, 0) + 1);
+                        }
+                    }
+                }
+                LOCK.notify();
+            }
+        }
+    }
+
+    private static String getReadLine(List<BufferedReader> readers) {
+        try {
+            for (BufferedReader bReader : readers) {
+                String line;
+                if ((line = bReader.readLine()) != null) {
+                    return line;
+                }
+            }
+            finishRead = true;
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("IOException when BufferedReader readLine!");
+        }
+    }
+
+    private static void closeReaders(List<BufferedReader> readers) throws IOException {
+        for (BufferedReader bReader : readers) {
+            bReader.close();
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
@@ -1,8 +1,92 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
 public class MultiThreadWordCount5 {
-    // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    // (ForkJoinPool) 使用threadNum个线程，并发统计文件中各单词的数量
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException {
+        Map<String, Integer> results = new LinkedHashMap<>();
+        List<WordsInFile> tasks = new ArrayList<>(files.size());
+        List<BufferedReader> readers = new ArrayList<>(files.size());
+
+        ForkJoinPool pool = new ForkJoinPool(threadNum);
+        for (File file : files) {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            readers.add(reader);
+            WordsInFile process = new WordsInFile(reader);
+            pool.execute(process);
+            tasks.add(process);
+        }
+
+        joinTaskResults(results, tasks);
+        closeReaders(readers);
+        return results;
+    }
+
+    public static class WordsInFile extends RecursiveTask<Map<String, Integer>> {
+        private static final int THRESHOLD = 20;
+        private int countLine = 0;
+        private BufferedReader reader;
+
+        public WordsInFile(BufferedReader reader) {
+            this.reader = reader;
+        }
+
+        @Override
+        protected Map<String, Integer> compute() {
+            Map<String, Integer> result = new LinkedHashMap<>();
+            List<WordsInFile> tasks = new ArrayList<>();
+            try {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    countLine += 1;
+                    for (String word : line.split(" ")) {
+                        if (!"".equals(word)) {
+                            result.put(word, result.getOrDefault(word, 0) + 1);
+                        }
+                    }
+
+                    if (countLine >= THRESHOLD) {
+                        WordsInFile task = new WordsInFile(reader);
+                        task.fork();
+                        tasks.add(task);
+                    }
+                    joinTaskResults(result, tasks);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+    }
+
+    private static void joinTaskResults(Map<String, Integer> results, List<WordsInFile> tasks) {
+        for (WordsInFile item : tasks) {
+            Map<String, Integer> map = item.join();
+            mergeToFinalMap(results, map);
+        }
+    }
+
+    private static void mergeToFinalMap(Map<String, Integer> results, Map<String, Integer> map) {
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String word = entry.getKey();
+            results.put(word, results.getOrDefault(word, 0) + entry.getValue());
+        }
+    }
+
+    private static void closeReaders(List<BufferedReader> readers) throws IOException {
+        for (BufferedReader bReader : readers) {
+            bReader.close();
+        }
+    }
 }
