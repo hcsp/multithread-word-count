4bdac3ec58128f99798eb3cde7ac7d50a1c878d9
yang song <1479867360@qq.com>
2020-12-21 14:02:23 +0800

    线程池实现Word Count (#183)
    线程池实现Word Count (#183)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,8 @@
 package com.github.hcsp.multithread;
 
-import java.io.File;
-import java.util.List;
-import java.util.Map;
-
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
-    }
+    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
+    //        return null;
+    //    }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,52 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
 public class MultiThreadWordCount2 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException, ExecutionException, InterruptedException {
+        MultiThreadWordCount2 count2 = new MultiThreadWordCount2();
+        ArrayList<Map<String, Integer>> maps = new ArrayList<>();
+        for (File file : files) {
+            maps.add(count2.fileCount(threadNum, file));
+        }
+        return CountTools.mapListReduce(maps);
+    }
+
+    private Map<String, Integer> fileCount(int threadNum, File file) throws IOException, ExecutionException, InterruptedException {
+        ArrayList<Map<String, Integer>> maps = new ArrayList<>();
+        BufferedReader reader = null;
+        reader = new BufferedReader(new FileReader(file));
+        ArrayList<Future<Map<String, Integer>>> futures = new ArrayList<>();
+        ExecutorService threadPool = newFixedThreadPool(threadNum);
+
+        for (int i = 0; i < threadNum; i++) {
+            BufferedReader finalReader = reader;
+            futures.add(threadPool.submit(() -> CountTools.lineToMap(finalReader)));
+        }
+
+        for (Future<Map<String, Integer>> future : futures) {
+            maps.add(future.get());
+        }
+        threadPool.shutdown();
+        return CountTools.mapListReduce(maps);
+    }
+
+//    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
+//        File directory = new File("D:\\temp");
+//        List<File> files = Arrays.asList(directory.listFiles());
+//        System.out.println(count(10, files));
+//        System.out.println();
+//    }
 }
