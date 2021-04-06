1f563ff8a0204b187792d16f57ed9c5ccdceaf50
George <jobsteng@163.com>
2021-04-06 21:12:12 +0800

    实现一个多线程的Word Count

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MultiThreadWordCount1 {
@@ -33,26 +35,28 @@ public class MultiThreadWordCount1 {
                     for (String line : lines) {
                         String[] words = line.split(" ");
                         for (String word : words) {
-                            addWord(word);
+                            synchronized (lock) {
+                                countRes.put(word, countRes.getOrDefault(word, 0) + 1);
+                            }
                         }
                     }
-                    if(fileIndex == 0)
-                    {
+                }
+                if (fileIndex == 0) {
+                    synchronized (lock) {
                         lock.notify();
                     }
                 }
-            });
+            }).start();
         }
-        if(filesSize.get() >= 0)
-        {
-
+        if (filesSize.get() > 0) {
+            try {
+                synchronized (lock) {
+                    lock.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         return countRes;
     }
-
-
-    public synchronized static void addWord(String word) {
-        countRes.put(word, countRes.getOrDefault(word, 0) + 1);
-    }
-
 }
