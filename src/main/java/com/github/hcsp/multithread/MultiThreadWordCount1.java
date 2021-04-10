568f4791ae8b3fa619d60e73d26ee2302841a1ad
Diesel Niu <niu853273494@qq.com>
2021-04-10 22:22:51 +0800

    提交作业，此题需要再次练习 (#188)
    提交作业，此题需要再次练习 (#188)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,58 @@
 package com.github.hcsp.multithread;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.*;
 
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws ExecutionException, InterruptedException, FileNotFoundException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+
+        for (File file : files) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            for (int i = 0; i < threadNum; i++) {
+                futures.add(threadPool.submit(new WorkerJob(bufferedReader)));
+            }
+        }
+
+        Map<String, Integer> finalResult = new HashMap<>();
+
+        for (Future<Map<String, Integer>> future : futures) {
+            mergeWorkerResultIntoFinalResult(future.get(), finalResult);
+        }
+        return finalResult;
+    }
+
+    private static void mergeWorkerResultIntoFinalResult(Map<String, Integer> resultFromWorker, Map<String, Integer> finalResult) {
+        for (Map.Entry<String, Integer> entry : resultFromWorker.entrySet()) {
+            int totalCount = finalResult.getOrDefault(entry.getKey(), 0) + entry.getValue();
+            finalResult.put(entry.getKey(), totalCount);
+        }
+    }
+
+    private static class WorkerJob implements Callable<Map<String, Integer>> {
+        private BufferedReader reader;
+
+        WorkerJob(BufferedReader reader) {
+            this.reader = reader;
+        }
+
+        @Override
+        public Map<String, Integer> call() throws Exception {
+            Map<String, Integer> result = new HashMap<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] wordList = line.split(" ");
+                for (String word : wordList) {
+                    result.put(word, result.getOrDefault(word, 0) + 1);
+                }
+            }
+            return result;
+        }
     }
 }
