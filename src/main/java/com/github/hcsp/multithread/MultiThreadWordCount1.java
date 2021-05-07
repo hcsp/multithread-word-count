157065b73719aa5fec22d7078becb2b3424eaaa1
zhangxiaoxi3 <zhangxiaoxi3@jd.com>
2021-05-07 23:45:46 +0800

    线程池计算

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,57 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
 
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws Exception {
+        Map<String, Integer> map = new HashMap<>();
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+        ExecutorService threadPool = newFixedThreadPool(threadNum);
+        for (File file : files) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            futures.add(threadPool.submit(new Task(bufferedReader)));
+        }
+        for (Future<Map<String, Integer>> future : futures) {
+            for (Map.Entry<String, Integer> entry : future.get().entrySet()) {
+                Integer num = map.getOrDefault(entry.getKey(), 0);
+                map.put(entry.getKey(), num + entry.getValue());
+            }
+        }
+        threadPool.shutdown();
+        return map;
+    }
+
+    static class Task implements Callable<Map<String, Integer>> {
+        private BufferedReader bufferedReader;
+
+        public Task(BufferedReader bufferedReader) {
+            this.bufferedReader = bufferedReader;
+        }
+
+        @Override
+        public Map<String, Integer> call() throws Exception {
+            String strLine = null;
+            Map<String, Integer> countMap = new ConcurrentHashMap<>();
+            while (null != (strLine = bufferedReader.readLine())) {
+                String[] words = strLine.split("\\s");
+                for (String word : words) {
+                    countMap.put(word, (countMap.getOrDefault(word, 0)+1));
+                }
+            }
+            return countMap;
+        }
     }
 }
