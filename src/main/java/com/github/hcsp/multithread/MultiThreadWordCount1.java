72519a780fd99e15daa2b1083dd4fc196baf5cbe
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-07-12 21:41:38 +0800

    feat: complete first MultiThreadWordCound (#196)
    feat: complete first MultiThreadWordCound (#196)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,42 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MultiThreadWordCount1 {
-    // 使用threadNum个线程，并发统计文件中各单词的数量
+public class MultiThreadWordCount1 extends Thread {
+    public static Map<String, Integer> resultMap = new HashMap<>();
+
     public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+        insertContentToMapFromFile(files);
+        return resultMap;
+    }
+
+    private static void insertContentToMapFromFile(List<File> fileList) {
+        for (File file : fileList) {
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                while (true) {
+                    String line = bufferedReader.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                    String[] wordList = line.split("\\s+");
+                    for (String word : wordList) {
+                        if (resultMap.containsKey(word)) {
+                            resultMap.put(word, resultMap.get(word) + 1);
+                        } else {
+                            resultMap.put(word, 1);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
