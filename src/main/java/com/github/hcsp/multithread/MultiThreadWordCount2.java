eca8d26b49c518e6bf3c186cfcd6b8aa11f97e9b
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-07-13 21:37:03 +0800

    ThreadPool & Callable & Future + Object.wait() & Object.notify() + CountDownLatch (#197)
    ThreadPool & Callable & Future + Object.wait() & Object.notify() + CountDownLatch (#197)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,49 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class MultiThreadWordCount2 {
-    // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        Map<String, Integer> resultMap = new ConcurrentHashMap<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(threadNum);
+        for (File file : files) {
+            Future submit = executorService.submit(new Temp(file));
+            try {
+                ConcurrentHashMap<String, Integer> result = (ConcurrentHashMap<String, Integer>) submit.get();
+                System.out.println("result = " + result);
+                result.keySet().forEach(key -> {
+                    if (resultMap.containsKey(key)) {
+                        resultMap.put(key, resultMap.get(key) + result.get(key));
+                    } else {
+                        resultMap.put(key, result.get(key));
+                    }
+                });
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return resultMap;
+    }
+
+    private static class Temp implements Callable {
+        private final File FILE;
+
+        Temp(File file) {
+            this.FILE = file;
+        }
+
+        @Override
+        public Map<String, Integer> call() {
+            return new ProcessFile(FILE).processFile();
+        }
+    }
 }
