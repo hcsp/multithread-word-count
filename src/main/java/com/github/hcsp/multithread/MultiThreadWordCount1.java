4af1eb051c75da2e47a52ed5bde760ca4696c9e2
Nicklzy <tydx11@hotmail.com>
2021-12-28 22:00:56 +0800

    线程池 (#206)
    线程池 (#206)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,40 @@
 package com.github.hcsp.multithread;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.*;
 
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException, ExecutionException, InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
+
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+        for (File file : files) {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            futures.add(threadPool.submit(() -> {
+                String line;
+                Map<String, Integer> map = new HashMap<>();
+                while ((line = reader.readLine()) != null) {
+                    String[] words = line.split(" ");
+                    for (String word : words) {
+                        map.put(word, map.getOrDefault(word, 0) + 1);
+                    }
+                }
+                return map;
+            }));
+        }
+        Map<String, Integer> resultMap = new HashMap<>();
+        for (Future<Map<String, Integer>> future : futures) {
+            Map<String, Integer> futureResult = future.get();
+            for (Map.Entry<String, Integer> item : futureResult.entrySet()) {
+                resultMap.put(item.getKey(), resultMap.getOrDefault(item.getKey(), 0) + item.getValue());
+            }
+        }
+
+        return resultMap;
     }
 }
