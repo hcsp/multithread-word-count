c98a40b74b8be02930afa727ff006fa149b84537
soohea <sooheaa@gmai.com>
2022-02-23 15:40:08 +0800

    Future与线程池&CountDownLatch&ForkJoinPool()&parallelStream()&CyclicBarrier

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount3.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount3.java
@@ -1,8 +1,36 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
 public class MultiThreadWordCount3 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    // 使用ForkJoinPool()
+
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        ForkJoinPool pool = new ForkJoinPool(threadNum);
+        List<Map<String, Integer>> mapList = new CopyOnWriteArrayList<>();
+        for (File file : files) {
+            Map<String, Integer> map = pool.invoke(new MyTask(file));
+            mapList.add(map);
+        }
+        return CountUtil.mergeFileResult(mapList);
+
+
+    }
+
+    static class MyTask extends RecursiveTask<Map<String, Integer>> {
+        File file;
+        MyTask(File file) {
+            this.file = file;
+        }
+        @Override
+        protected Map<String, Integer> compute() {
+            return CountUtil.countOneFile(file);
+        }
+    }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount4.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount4.java
@@ -1,8 +1,23 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class MultiThreadWordCount4 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    // 使用parallelStream()
+
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        String threadNumStr = String.valueOf(threadNum);
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", threadNumStr);
+        List<Map<String, Integer>> mapList = new CopyOnWriteArrayList<>();
+        files.parallelStream().forEach(file -> {
+            Map<String, Integer> oneMap = CountUtil.countOneFile(file);
+            mapList.add(oneMap);
+        });
+        return CountUtil.mergeFileResult(mapList);
+    }
+
 }
