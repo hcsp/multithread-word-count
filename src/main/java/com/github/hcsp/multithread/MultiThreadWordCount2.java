3dbae6d7dc9b19d0702ed01fee66ba483359a99c
soohea <77014206+soohea@users.noreply.github.com>
2022-02-23 15:52:25 +0800

    Future与线程池&CountDownLatch&ForkJoinPool()&parallelStream()&CyclicBarrier (#207)
    Future与线程池&CountDownLatch&ForkJoinPool()&parallelStream()&CyclicBarrier (#207)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,31 @@
 package com.github.hcsp.multithread;
 
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+
+
 public class MultiThreadWordCount2 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    // 使用CountDownLatch
+
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        List<Map<String, Integer>> mapList = new CopyOnWriteArrayList<>();
+        for (File file : files) {
+            new Thread(() -> {
+                Map<String, Integer> oneMap = CountUtil.countOneFile(file);
+                mapList.add(oneMap);
+                countDownLatch.countDown();
+            }).start();
+        }
+        countDownLatch.await();
+        return CountUtil.mergeFileResult(mapList);
+
+    }
 }
+
+
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount5.java
@@ -1,8 +1,39 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CyclicBarrier;
+
 public class MultiThreadWordCount5 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    // 使用CyclicBarrier
+
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(threadNum + 1);
+        List<Map<String, Integer>> mapList = new CopyOnWriteArrayList<>();
+        for (File file : files) {
+            new Thread(() -> {
+                Map<String, Integer> oneMap = CountUtil.countOneFile(file);
+                mapList.add(oneMap);
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+
+            }).start();
+
+        }
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        return CountUtil.mergeFileResult(mapList);
+    }
+
+
 }
