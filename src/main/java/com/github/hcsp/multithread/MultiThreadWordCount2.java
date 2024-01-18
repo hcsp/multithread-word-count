027473bb7254a36d13ae18dddebc0cc59e3f5cab
Leo Cheung <leozhang621@gmail.com>
2024-01-18 10:59:43 +0800

    fix the compatibility issue of ForkJoinPoll in lower versions of JDK

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -14,11 +14,10 @@ import java.util.concurrent.RecursiveTask;
 public class MultiThreadWordCount2 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
     public static Map<String, Integer> count(int threadNum, List<File> files) throws ExecutionException, InterruptedException {
-        try (ForkJoinPool pool = new ForkJoinPool(threadNum)) {
-            ForkJoinTask<Map<String, Integer>> task = new Task(files);
-            pool.submit(task);
-            return task.get();
-        }
+        ForkJoinPool pool = new ForkJoinPool(threadNum);
+        ForkJoinTask<Map<String, Integer>> task = new Task(files);
+        pool.submit(task);
+        return task.get();
     }
 
     public static class Task extends RecursiveTask<Map<String, Integer>> {
