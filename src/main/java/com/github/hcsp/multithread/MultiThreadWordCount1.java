389168db63e1c75918c149064c3a2a4d756b8b08
Leo Cheung <leozhang621@gmail.com>
2024-01-18 11:01:50 +0800

    implement WordCount using 3 different methods (#222)
    implement WordCount using 3 different methods (#222)

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount1.java
@@ -1,12 +1,55 @@
 package com.github.hcsp.multithread;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 public class MultiThreadWordCount1 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    public static Map<String, Integer> count(int threadNum, List<File> files) {
-        return null;
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws InterruptedException, ExecutionException {
+        List<Future<Map<String, Integer>>> futureTaskList = new ArrayList<>();
+        for (int i = 0; i < threadNum; i++) {
+            FutureTask<Map<String, Integer>> task = new FutureTask<>(new Worker(files.get(i)));
+            futureTaskList.add(task);
+            new Thread(task).start();
+        }
+        List<Map<String, Integer>> workerResults = new ArrayList<>();
+        for (Future<Map<String, Integer>> task : futureTaskList) {
+            workerResults.add(task.get());
+        }
+        return mergeWorkerResults(workerResults);
+    }
+
+    public static class Worker implements Callable<Map<String, Integer>> {
+        private final File file;
+
+        public Worker(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public Map<String, Integer> call() throws Exception {
+            Map<String, Integer> result = new HashMap<>();
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] words = line.split(" ");
+                    for (String word : words) {
+                        result.put(word, result.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    private static Map<String, Integer> mergeWorkerResults(List<Map<String, Integer>> workerResults) {
+        return CommonUtils.mergeWorkerResults(workerResults);
     }
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,78 @@
 package com.github.hcsp.multithread;
 
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
 public class MultiThreadWordCount2 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws ExecutionException, InterruptedException {
+        ForkJoinPool pool = new ForkJoinPool(threadNum);
+        ForkJoinTask<Map<String, Integer>> task = new Task(files);
+        pool.submit(task);
+        return task.get();
+    }
+
+    public static class Task extends RecursiveTask<Map<String, Integer>> {
+        private final List<File> files;
+
+        public Task(List<File> files) {
+            this.files = files;
+        }
+
+        @Override
+        protected Map<String, Integer> compute() {
+            if (files.size() == 1) {
+                try {
+                    return doWork();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            List<File> subFiles1 = new ArrayList<>();
+            List<File> subFiles2 = new ArrayList<>();
+            for (int i = 0; i < files.size(); i++) {
+                File file = files.get(i);
+                if (i % 2 == 0) {
+                    subFiles1.add(file);
+                } else {
+                    subFiles2.add(file);
+                }
+            }
+            Task task1 = new Task(subFiles1);
+            Task task2 = new Task(subFiles2);
+            invokeAll(task1, task2);
+            return mergeTask(task1.join(), task2.join());
+        }
+
+        private Map<String, Integer> doWork() throws IOException {
+            Map<String, Integer> result = new HashMap<>();
+            try (BufferedReader reader = new BufferedReader(new FileReader(files.get(0)))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] words = line.split(" ");
+                    for (String word : words) {
+                        result.put(word, result.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static Map<String, Integer> mergeTask(Map<String, Integer> t1, Map<String, Integer> t2) {
+            Map<String, Integer> result = new HashMap<>(t1);
+            for (Map.Entry<String, Integer> entry : t2.entrySet()) {
+                result.put(entry.getKey(), result.getOrDefault(entry.getKey(), 0) + entry.getValue());
+            }
+            return result;
+        }
+    }
+
 }
