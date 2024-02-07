6a65584bc546ce68e56bd6f324bc86c6ad3cb7ca
dexter_git <runfenglin@qq.com>
2024-02-07 17:34:46 +0800

    multithread-word-count

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount4.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount4.java
@@ -1,8 +1,79 @@
 package com.github.hcsp.multithread;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+import java.util.concurrent.*;
+
 public class MultiThreadWordCount4 {
+    //Future and ThreadPool
+    //每个线程完成任务,并返回Future值,形成一个List<Future<Map<String,Integer>>>, 最后统一处理
+    private static final List<Future<Map<String,Integer>>> futures = new ArrayList<>();
+
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+        public static Map<String, Integer> count(int threadNum, List<File> files) {
+            final ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
+            int chunkNum = files.size() / threadNum;
+
+            for (int i = 0; i < threadNum; i++) {
+                int begin = i * chunkNum;
+                int end = i != threadNum - 1? (i + 1 ) * chunkNum : files.size();
+                futures.add(threadPool.submit(new ReadAndCount(files.subList(begin, end))));
+            }
+            threadPool.shutdown();
+            return changeFutures();
+        }
+
+    private static Map<String, Integer> changeFutures() {
+        Map<String, Integer> result = new HashMap<>();
+        for (Future<Map<String, Integer>> future:
+                MultiThreadWordCount4.futures) {
+            try {
+                mergeFutureIntoResult(future.get(), result);
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+    }
+
+    private static void mergeFutureIntoResult(Map<String, Integer> stringIntegerMap, Map<String, Integer> result) {
+        for (Map.Entry<String,Integer> map:
+             stringIntegerMap.entrySet()) {
+            result.put(map.getKey(), result.getOrDefault(map.getKey(), 0) + map.getValue());
+        }
+    }
+
+    private static class ReadAndCount implements Callable<Map<String, Integer>> {
+
+        private final List<File> inputFiles;
+
+        public ReadAndCount(List<File> inputFiles) {
+            this.inputFiles = inputFiles;
+        }
+
+        @Override
+        public Map<String, Integer> call() throws Exception {
+            Map<String,Integer> result = new HashMap<>();
+            for (File file:
+                 inputFiles) {
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                    String  line;
+                    while((line = bufferedReader.readLine()) != null){
+                        mergeLineIntoResult(line, result);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void mergeLineIntoResult(String line, Map<String, Integer> result) {
+            String[] words = line.split(" ");
+            for (String word:
+                 words) {
+                result.put(word, result.getOrDefault(word, 0) + 1);
+            }
+        }
+    }
 }
---
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount6.java
@@ -0,0 +1,41 @@
+package com.github.hcsp.multithread;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MultiThreadWordCount6 {
+    //Collection.parallelStream()
+    //按照线程数分配给各个任务,并行的执行
+    // 使用threadNum个线程，并发统计文件中各单词的数量
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        return  files.parallelStream().map(MultiThreadWordCount6::readAndCount).reduce(new HashMap<>(),MultiThreadWordCount6::mergeMap);
+    }
+    private static Map<String, Integer> readAndCount(File file) {
+        Map<String, Integer> result = new HashMap<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                Arrays.stream(line.split(" ")).forEach(word -> result.merge(word, 1, Integer::sum));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return  result;
+    }
+    private static Map<String, Integer> mergeMap(Map<String, Integer> map1, Map<String, Integer> map2) {
+        Map<String,Integer> mergeResult = new HashMap<>();
+        map1.forEach((key,value)-> mergeResult.merge(key,value, Integer::sum));
+        map2.forEach((key,value)-> mergeResult.merge(key,value, Integer::sum));
+        return  mergeResult;
+    }
+
+
+
+
+}
